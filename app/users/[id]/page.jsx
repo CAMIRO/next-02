9baf9afea4bf31128a1cce47afd9a7d276e27e20
CAMIRO <juancamilosc@hotmail.com>
2023-09-03 18:14:03 -0500

    adding more details in the first User section

--- app/users/[id]/page.jsx
+++ app/users/[id]/page.jsx
@@ -8,8 +8,11 @@ async function getUser(id) {
 export default async function UserPage({params}) {
   
   const user = await getUser(params.id)
-  console.log("🚀  user:", user)
   return (
-    <div>User Page</div>
+    <div className="bg-slate-400 p-10 rounded-md">
+      <img src={user.avatar} alt={user.first_name} className="m-auto my-4"/>
+      <h3 className="text-3xl font-bold">{user.id} {user.first_name} {user.last_name}</h3>
+      <p>email: {user.email}</p>
+    </div>
   )
 }
